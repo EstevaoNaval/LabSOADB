319b54d8fd42820706d38798d5c2b29f421cb202
Estevao Naval <navalestevao@gmail.com>
2025-03-14 13:45:57 -0300

    - Subindo api para revogar celery task;

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserTaskReadOnlyViewSet
+from .views import UserTaskReadOnlyViewSet, RevokeTaskAPIView
 
 urlpatterns = [
     path('user/<int:user_id>/', UserTaskReadOnlyViewSet.as_view({'get': 'list'}), name="user-tasks"),
+    path('revoke/', RevokeTaskAPIView, name="revoke-task")
 ]
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,6 +1,10 @@
+from celery import current_app
+
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from user.models import User
 from .models import UserTask
@@ -15,3 +19,27 @@ class UserTaskReadOnlyViewSet(ReadOnlyModelViewSet):
         user_id = self.kwargs.get('user_id')
         user = get_object_or_404(queryset=User, id=user_id)
         return UserTask.objects.filter(user=user)
+
+class RevokeTaskAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, format=None):
+        task_id = request.data.get("task_id")
+        
+        if not task_id:
+            return Response(data={"error": "task_id is a required attribute"}, status=400)
+        
+        user = User.objects.get(id=request.user.id)
+        
+        user_task = UserTask.objects.filter(user=user, task_id=task_id)
+        
+        if user_task.exists():
+            current_app.control.revoke(task_id, terminate=True, signal='SIGKILL')
+            user_task.update(status='REVOKED')
+        else:
+            return Response(data={"message": "Task not found"}, status=404)
+        
+        return Response(status=200)
+        
+    
+    
