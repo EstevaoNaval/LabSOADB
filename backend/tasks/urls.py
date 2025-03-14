109038961d653137722d54503120b1b4808ffbbf
Estevao Naval <navalestevao@gmail.com>
2025-03-14 14:34:51 -0300

    - Corrigindo declaração do enpoint para revogação das tasks do celery;

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -3,5 +3,5 @@ from .views import UserTaskReadOnlyViewSet, RevokeTaskAPIView
 
 urlpatterns = [
     path('user/<int:user_id>/', UserTaskReadOnlyViewSet.as_view({'get': 'list'}), name="user-tasks"),
-    path('revoke/', RevokeTaskAPIView, name="revoke-task")
+    path('revoke/', RevokeTaskAPIView.as_view(), name="revoke-task")
 ]
