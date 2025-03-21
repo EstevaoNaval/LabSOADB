3bc17c164e5d3454e7ba414adb51fc9a387387f0
Estevao Naval <navalestevao@gmail.com>
2025-03-21 17:16:30 -0300

    - Ordenando viewset UserChemicalsReadOnlyViewSet pelo último químico criado;

--- backend/chemicals/views.py
+++ backend/chemicals/views.py
@@ -151,7 +151,7 @@ class ChemicalPropListView(generics.ListAPIView):
 class UserChemicalsReadOnlyViewSet(ReadOnlyModelViewSet):
     serializer_class = UserChemicalSerializer
     permission_classes = [IsAuthenticated]
-    ordering = ['created_at']
+    ordering = ['-created_at']
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     
     def get_queryset(self):
