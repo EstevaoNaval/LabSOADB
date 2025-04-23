24665493ffa22385d2e02ee0aa38fb487d48e5bf
Estevao Naval <navalestevao@gmail.com>
2025-04-23 20:17:03 -0300

    Atualizações 2025-04-23 08:17 PM - Subindo remoção do atributo export_task do modelo UserTask;

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -54,13 +54,5 @@ class UserTask(models.Model):
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    export_task = models.OneToOneField(
-        ExportJob,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name="user_tasks"
-    )
-
     def __str__(self):
         return f"{self.task_name} ({self.task_id}) - {self.status}"
