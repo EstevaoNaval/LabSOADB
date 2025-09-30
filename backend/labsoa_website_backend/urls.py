45f456f70fbc0d838feb8f3c079ca8066ed954db
Estevao Naval <navalestevao@gmail.com>
2025-09-30 02:37:06 -0300

    - Adicionando prefixo api em todas as urls de autenticação no backend;

--- backend/labsoa_website_backend/urls.py
+++ backend/labsoa_website_backend/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/chemicals/", include("chemicals.urls")),
     path("api/pdf2chemicals/", include("pdf2chemicals_service.urls")),
-    path("auth/", include("authentication.urls")),
+    path("api/auth/", include("authentication.urls")),
     path("api/tasks/", include("tasks.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
