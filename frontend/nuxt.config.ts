6ef925aca3ca093fe1a260a845c6b429b1c4d134
Estevao Naval <navalestevao@gmail.com>
2025-09-30 01:14:56 -0300

    - Corrigindo erro de sintaxe em nuxt.config.ts;

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -22,11 +22,12 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-  routeRules: {
-    '/api/**': { 
-      proxy: process.env.NUXT_API_URL_HOST 
-        ? `${process.env.NUXT_API_URL_HOST}/**`
-        : 'http://localhost:8000/**'  // Fallback para tunnel local
+    routeRules: {
+      '/api/**': { 
+        proxy: process.env.NUXT_API_URL_HOST 
+          ? `${process.env.NUXT_API_URL_HOST}/**`
+          : 'http://localhost:8000/**'  // Fallback para tunnel local
+      }
     }
   },
 
