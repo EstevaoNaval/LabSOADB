807659b51561c89e7b1c4186645dd7122654b796
Estevao Naval <navalestevao@gmail.com>
2025-09-30 14:15:31 -0300

    - Removendo adição explícita do config.public.apiHost no link para redirecionar a api docs;

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -52,7 +52,7 @@
                     </a>
                 </li>
                 <li>
-                    <a class="flex hover:text-primary" :href="docsAPIUrl" rel="noopener noreferrer">
+                    <a class="flex hover:text-primary" :href="config.public.docsAPIEndpoint" rel="noopener noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
                         </svg>
@@ -301,7 +301,7 @@
                                 </NuxtLink>
                             </li>
                             <li>
-                                <a class="flex" :href="docsAPIUrl" rel="noopener noreferrer">
+                                <a class="flex" :href="config.public.docsAPIEndpoint" rel="noopener noreferrer">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
                                     </svg>
@@ -417,8 +417,6 @@ const config = useRuntimeConfig()
 const { $enter, $afterEnter, $leave, $afterLeave } = useNuxtApp()
 
 const router = useRouter()
-
-const docsAPIUrl = config.public.apiHost + config.public.docsAPIEndpoint
 
 const themeStore = useThemeStore();
 const authStore = useAuthStore()
