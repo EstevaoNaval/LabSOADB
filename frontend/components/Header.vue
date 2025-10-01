a36f94825b53d581d8d090a049bed28419871e06
Estevao Naval <navalestevao@gmail.com>
2025-09-30 22:26:31 -0300

    - Transformando link para docs da API da tag <a> para <NuxtLink>;

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -301,12 +301,12 @@
                                 </NuxtLink>
                             </li>
                             <li>
-                                <a class="flex" :href="config.public.docsAPIEndpoint" rel="noopener noreferrer">
+                                <NuxtLink class="flex" :to="config.public.docsAPIEndpoint">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
                                     </svg>
                                     Web API
-                                </a>
+                                </NuxtLink>
                             </li>
                         </ul>
                     </div>
