5e86ef1c44948b32326fac7fb7e47394f24e5620
Estevao Naval <navalestevao@gmail.com>
2026-01-18 17:52:19 -0300

    BUGFIX: pdf2chemicals api revoke removal - Removing desnecessary pdf2chemicals task revoke api as already exists a general API to revoke any tasks in tasks app;

--- backend/pdf2chemicals_service/urls.py
+++ backend/pdf2chemicals_service/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import (
-    PDFUploadView,
-    PDF2ChemicalsTaskRevokeView
+    PDFUploadView
 )
 
 urlpatterns = [
-    path('submit/', PDFUploadView.as_view(), name='pdf2chemicals-submit'),
-    path('revoke/<str:task_id>/', PDF2ChemicalsTaskRevokeView.as_view, 'revoke-pdf2chemicals-task')
+    path('submit/', PDFUploadView.as_view(), name='pdf2chemicals-submit')
 ]
