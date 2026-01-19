6ccecd7bc98759d997f3a8bd7a054c902d181c1e
Estevao Naval <navalestevao@gmail.com>
2026-01-18 22:54:59 -0300

    BUGFIX: pdf2chemicals_service.util.tasks - Subindo correção na passagem de dados entre as tasks de pdf2chemicals_service;

--- backend/pdf2chemicals_service/util/tasks.py
+++ backend/pdf2chemicals_service/util/tasks.py
@@ -27,8 +27,8 @@ class ChainedTask(AbortableTask):
         Merge dict args into kwargs (your ChainedTask pattern).
         Allows tasks to pass data through chain as single dict.
         """
-        if len(args) == 1 and isinstance(args, dict):
-            kwargs.update(args)
+        if len(args) == 1 and isinstance(args[0], dict):
+            kwargs.update(args[0])
             args = ()
         return super(ChainedTask, self).__call__(*args, **kwargs)
     
